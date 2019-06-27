c58ad0000a62ca754841af89e23838766954b857
Maewmiie <memy090909@gmail.com>
2019-06-27 14:20:19 +0700

    init

---
+++ common/models/User.php
@@ -0,0 +1,278 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\mongodb\ActiveRecord;
+use yii\web\IdentityInterface;
+
+/**
+ * User model
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $verification_token
+ * @property string $email
+ * @property string $auth_key
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $password write-only password
+ */
+class User extends ActiveRecord implements IdentityInterface
+{
+    const STATUS_DELETED = 0;
+    const STATUS_INACTIVE = 10;
+    const STATUS_ACTIVE = 10;
+
+    const ROLE_USER = 'user';
+    const ROLE_MOD = 'mod';
+    const ROLE_ADMIN = 'admin';
+
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public static function tableName()
+    // {
+    //     return '{{%user}}';
+    // }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function CollectionName()
+    {
+        return ['yii_ericsson', 'user'];
+    }
+    
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public function behaviors()
+    // {
+    //     return [
+    //         TimestampBehavior::className(),
+    //         //adding
+    //         'attributes' => [
+    //             ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+    //             ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+    //         ],
+    //     ];
+    // }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'Timestamp' => [
+                'class' => 'yii\behaviors\TimestampBehavior',
+                 //adding
+            'attributes' => [
+                ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+            ],
+            ]
+        ];
+
+        // return [
+        //             TimestampBehavior::className(),
+        //             //adding
+        //             'attributes' => [
+        //                 ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+        //                 ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+        //             ],
+        //         ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['status', 'default', 'value' => self::STATUS_INACTIVE],
+            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE]],
+            ['role', 'default', 'value' => self::ROLE_USER],
+            ['role', 'in', 'range' => [self::ROLE_USER, self::ROLE_ADMIN]],
+        ];
+    }
+    //////เพิ่มตัวแปร
+    public function attributes()
+    {
+        return [
+            '_id',
+            'username',
+            'password_hash',
+            'password_reset_token',
+            'verification_token',
+            'email',
+            'auth_key',
+            'role',
+            'status',
+            'created_at',
+            'updated_at'
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        // return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne($id);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * Finds user by password reset token
+     *
+     * @param string $token password reset token
+     * @return static|null
+     */
+    public static function findByPasswordResetToken($token)
+    {
+        if (!static::isPasswordResetTokenValid($token)) {
+            return null;
+        }
+
+        return static::findOne([
+            'password_reset_token' => $token,
+            'status' => self::STATUS_ACTIVE,
+        ]);
+    }
+
+    /**
+     * Finds user by verification email token
+     *
+     * @param string $token verify email token
+     * @return static|null
+     */
+    public static function findByVerificationToken($token) {
+        return static::findOne([
+            'verification_token' => $token,
+            'status' => self::STATUS_INACTIVE
+        ]);
+    }
+
+    /**
+     * Finds out if password reset token is valid
+     *
+     * @param string $token password reset token
+     * @return bool
+     */
+    public static function isPasswordResetTokenValid($token)
+    {
+        if (empty($token)) {
+            return false;
+        }
+
+        // $timestamp = (int) substr($token, strrpos($token, '_') + 1);
+        // $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+
+        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+        $parts = explode('', $token);
+        $timestamp = (int) end($parts);
+        return $timestamp + $expire >= time();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    /**
+     * Generates new password reset token
+     */
+    public function generatePasswordResetToken()
+    {
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
+//    ************************************************ token
+    public function generateEmailVerificationToken()
+    {
+        $this->verification_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
+
+    /**
+     * Removes password reset token
+     */
+    public function removePasswordResetToken()
+    {
+        $this->password_reset_token = null;
+    }
+}
---
+++ common/tests/unit/models/LoginFormTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace frontend\tests\unit\models;
+
+
+use Yii;
+use common\models\Loginform;
+use common\models\User;
+use common\fixtures\UserFixture;
+
+class loginForm2 extends \Codeception\Test\Unit
+{
+    public function testWrongUser()
+    {
+        $model = new LoginForm();
+
+        $model->attributes = [
+            'username' => 'maewmiie6',
+            'password' => '123456',
+        ];
+
+       expect('model should not login user', $model->login())->false();
+   
+    }
+
+    public function testCorrectUser()
+    {
+        $model = new LoginForm();
+        $model->attributes = [
+            'username' => 'maewmiie2',
+            'password' => '123456',
+        ];
+        expect('model should not login user', $model->login())->true();
+    }
+    public function CorrectUserWrongPass()
+    {
+        $model = new LoginForm();
+        $model->attributes = [
+            'username' => 'maewmiie2',
+            'password' => '123456',
+        ];
+        expect('model correct login user', $model->login())->false();
+    }
+    public function testFindToken()
+    {
+        $model = new User();
+        $model->attributes = [
+            'id' => 'maewmiie',
+        ];
+
+        expect('',$model->findIdentity($model->id));
+    }
+}
